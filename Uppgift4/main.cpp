b006d235485247fba38561c72548d9bb36882c3d
fredrikpettersson <fredrik.pettersson@gronahus.se>
2021-07-19 15:17:24 +0200

    Färdig med 4a

--- Uppgift4/main.cpp
+++ Uppgift4/main.cpp
@@ -1,57 +1,74 @@
 #include <iostream>
-#include <string>
 #include <cctype>
+#include <array>
 
 using namespace std;
 
+struct LETTER
+{
+    char letter;
+    int amount;
+};
+
 const int ANTAL_BOKSTAVER = 26;  //A-Z
 
-void berakna_histogram_abs(string basicString);
+void berakna_histogram_abs(string input, LETTER letter[ANTAL_BOKSTAVER]);
+
+void skriv_histogram_abs(LETTER pLetter[26]);
 
 int main()
 {
-   string input;
+    string input;
+    LETTER letter[ANTAL_BOKSTAVER];
 
-    cout << "Enter a string \n";
-    cin >> input;
+    cout << "Ge en rad med text: \n";
+    getline(cin, input);
+    berakna_histogram_abs(input, letter);
 
-    berakna_histogram_abs(input);
+   skriv_histogram_abs(letter);
 
     return 0;
 }
 
-void berakna_histogram_abs(string input) {
+void skriv_histogram_abs(LETTER letter[ANTAL_BOKSTAVER]) {
+    int num_of_letters = 0;
 
-    char letters[ANTAL_BOKSTAVER];
-    int count[ANTAL_BOKSTAVER];
+    //Går igenom listan och summerar ihop antalet bokstäver
+    for(int i = 0; i < ANTAL_BOKSTAVER; i++){
+        num_of_letters += letter[i].amount;
+    }
 
+    cout <<"\n";
+    cout << "Resultat för bokstäverna A-Z \n";
+
+    cout <<"Totala antalet bokstäver: "<< num_of_letters <<"\n";
+    cout << "Bokstavsfördelning:\n \n";
 
     for (int i = 0; i < ANTAL_BOKSTAVER; i++) {
-        count[i] = 0;
+        cout << letter[i].letter << " : " <<letter[i].amount << "\n";
     }
-    for (int i = 0; i < ANTAL_BOKSTAVER; i++)
-        letters[i] = ' ';
+}
 
-    for (int i = 0; i < input.size(); i++) {
+void berakna_histogram_abs(string input, LETTER letter[ANTAL_BOKSTAVER]) {
 
-        for (int j = 0; j < sizeof(letters); j++) {
+    //Går igenom hela listan och sätter position 0 till A, position 1 till B osv.
+    //Sätter även alla amount till 0
+    for (int i = 0; i < ANTAL_BOKSTAVER; i++) {
+        letter[i].amount = 0;
+        letter[i].letter = (char)(65 + i);
+    }
 
-            if (letters[j] == (char) tolower( input[i])) {
-                count[j]++;
-                break;
-            }
-            else if (letters[j] == ' ') {
-                letters[j] = input[i];
-                count[j]++;
+    //Går igenom hela inputen
+    for (char i : input) {
+
+        //För varje input går vi igenom letter listan
+        for (int j = 0; j < ANTAL_BOKSTAVER; j++) {
+
+            //Om bokstaven finns med så plussar vi på amount med +1
+            if (letter[j].letter == (char) toupper( i)) {
+                letter[j].amount++;
                 break;
             }
         }
-    }
-
-
-    int i = 0;
-    while(letters[i] != ' ') {
-        cout << letters[i] << " -> " << count[i] << "\n";
-        i++;
     }
 }
